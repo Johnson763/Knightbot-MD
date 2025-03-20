f4f7c491bae4edfe2de3eb90dca74492d29b4755
Johnson763 <104252347+Johnson763@users.noreply.github.com>
2025-03-20 23:29:50 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "JSN AIM",
+  botOwner: 'JSN AIM', // Your name
+  ownerNumber: '2349064040316', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
